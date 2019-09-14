cb000f14a8312385db6be34777d55c46b3d6eb07
Tami Reichold <reichold.tamara@gmail.com>
2019-09-14 09:35:06 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,6 @@
-def square_array(array)
-  counter = 0 
-  new_array = []
-  while counter < array.length do
-    new_array.push(array)
-    counter += 1
-    p new_array
-  end
+def square_array(ary)
+  a = Array.new(ary.length)
+  ary.each_index { |i| a[i] = ary[i] * ary[i] }
+  a
 end
+
